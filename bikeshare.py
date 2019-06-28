8606b1d0285fd394bac07ffc28d6dc76b87c2d15
Omar Ashary <omarashary@hotmail.de>
2019-06-29 00:27:05 +0200

    modified:   bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
@@ -202,6 +203,8 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         data_separation(df)
+
+#Allows code to be executed again        
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
